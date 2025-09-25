a51ad4cc387c7949d124aef3ec60da558a13e277
nailaamanda883 <nailaamanda883@gmail.com>
2025-09-25 13:08:45 +0700

    adatambahandarinaila

--- collabnyanailanaura.java
+++ collabnyanailanaura.java
@@ -1,5 +1,5 @@
 public class collabnyanailanaura {
     public static void main(String[] args) {
-        System.out.println("Hi, ini tambahan dari nailaaa");
+        System.out.println("Hi, ini ada tambahan dari naila");
     }
 }
